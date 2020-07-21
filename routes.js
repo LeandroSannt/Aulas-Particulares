9f436ff29b063f3015862e07dc0fc4c2da423d59
leandro santos do nascimento <lsn_slim@yahoo.com.br>
2020-07-21 08:41:01 -0300

    Criando Aulas particulares

---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const routes = express.Router()
+
+routes.get("/",function(req, res){
+    return res.redirect("/teachers")
+})
+
+routes.get("/teachers",function(req, res){
+    return res.render("teachers")
+})
+
+routes.get("/students", function(req, res){
+    return res.render ("students")
+            
+})
+
+routes.get("/not-found",function(req, res){
+    return res.render("not-found")
+})
+
+routes.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+  module.exports = routes
